f656e62027d68b2e19102057e0c64f158f2d0f38
Steven K. Terry <stkterry@gmail.com>
2019-08-22 12:21:31 -0400

    Updated nav links

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -17,14 +17,15 @@ class Nav extends React.Component {
         </div>
 
         <div className="nav-links">
-          <a href="#" className="nav-links-personal btn-raise">
+          <a href="https://steventerry.netlify.com/" className="nav-links-personal btn-raise">
             <h3>Steven Terry</h3>
             <i className="fas fa-portrait" />
           </a>
           <div className="nav-text-divider"></div>
           <a href="https://github.com/stkterry/quelea" className="btn-raise" target="_blank" rel="noopener noreferrer"><i className='fab fa-github-square' /></a>
-          <a href="#" className="btn-raise"><i className="fab fa-linkedin" /></a>
-          <a href="#" className="btn-raise"><i className="fas fa-envelope-square" /></a>
+          <a href="https://www.linkedin.com/in/steven-terry-59977999/" className="btn-raise" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin" /></a>
+          <a href="https://angel.co/stkterry" className="btn-raise" target="_blank" rel="noopener noreferrer"><i className="fab fa-angellist" /></a>
+          {/* <a href="#" className="btn-raise"><i className="fas fa-envelope-square" /></a> */}
         </div>
       </div>
     )
